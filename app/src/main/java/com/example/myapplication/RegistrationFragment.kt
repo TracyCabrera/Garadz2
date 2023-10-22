80bd6ba28235666cdd5d8038cbfb6525138d0f6b
Markus <markuskristofferc@gmail.com>
2023-10-23 00:01:27 +0800

    ufdate

--- app/src/main/java/com/example/myapplication/RegistrationFragment.kt
+++ app/src/main/java/com/example/myapplication/RegistrationFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class RegistrationFragment : AppCompatActivity() {
@@ -16,7 +17,7 @@ class RegistrationFragment : AppCompatActivity() {
         val username = findViewById(R.id.usernametextview) as EditText
         val newPassword = findViewById(R.id.newPassword) as EditText
         val confirmPassword = findViewById(R.id.confirmPassword) as EditText
-        val registerButton = findViewById(R.id.registerButton)  as Button
+        val registerButton = findViewById(R.id.registerButton) as Button
         val dbHelper = DBHelper(this)
 
         registerButton.setOnClickListener {
@@ -25,23 +26,33 @@ class RegistrationFragment : AppCompatActivity() {
             val confirmPassword = confirmPassword.text.toString()
             val savedata = dbHelper.insertdata(username, newPassword)
 
-            if (TextUtils.isEmpty(username) || TextUtils.isEmpty(newPassword) || TextUtils.isEmpty(confirmPassword)) {
-                Toast.makeText(
-                    this,
-                    "Add Username, Password & Confirm password",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                if (newPassword.equals(confirmPassword)) {
-                    if (savedata == true) {
-                        Toast.makeText(this, "SignUp Successful", Toast.LENGTH_SHORT).show()
-                        val intent = Intent(applicationContext, registerButton::class.java)
-                        startActivity(intent)
-                    } else {
-                        Toast.makeText(this, "User Exists", Toast.LENGTH_SHORT).show()
-                    }
+            val textView14 = findViewById<TextView>(R.id.textView14)
+            textView14.setOnClickListener {
+
+                val intent1 = Intent(this, RegistrationFragment::class.java)
+                startActivity(intent1)
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(newPassword) || TextUtils.isEmpty(
+                        confirmPassword
+                    )
+                ) {
+                    Toast.makeText(
+                        this,
+                        "Add Username, Password & Confirm password",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 } else {
-                    Toast.makeText(this, "Password Not Match", Toast.LENGTH_SHORT).show()
+                    if (newPassword.equals(confirmPassword)) {
+                        if (savedata == true) {
+                            Toast.makeText(this, "SignUp Successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(applicationContext, registerButton::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "User Exists", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(this, "Password Not Match", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
